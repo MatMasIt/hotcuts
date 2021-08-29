dd9640587d8243add82f403a2271d95ba2c6135a
MatMasIt <mattia.mascarello@outlook.it>
2021-08-30 00:32:28 +0200

    New user-based concise daemon

--- hotcutdaemon.py
+++ hotcutdaemon.py
@@ -1,12 +1,11 @@
+#!/usr/bin/python3
 from evdev import InputDevice, categorize, ecodes
-import configparser
+import configparser, os
 config = configparser.ConfigParser()
 config.read('config.ini')
 def sysrun(command):
-    f=open("lastCommand","w")
-    f.write(command)
-    f.close()
-dev = InputDevice('/dev/input/by-id/usb-dakai_PS_2+USB_Keyboard-event-kbd')
+    os.system(command+ " &")
+dev = InputDevice(config["DEVICE"]["path"])
 dev.grab()
 for event in dev.read_loop():
     if event.type == ecodes.EV_KEY:
